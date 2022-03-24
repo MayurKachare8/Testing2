7341995763923b68072f2b9fab68bb8d2a4f21d2
Mayur Kachare <mayur.kachare@zensar.com>
2022-03-24 16:30:23 +0530

    initial commit

---
+++ TestTrignometry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTrignometry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTrignometry")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("780cceec-f229-4f5d-938e-2b4cea4efa3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Trignometry/TrignometricOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trignometry
+{
+  public  class TrignometricOperations
+    {
+        public double AreaOfSqaure(double length)
+        {
+            return length * length;
+        }
+        public double AreaOfRectangle(double length, double width)
+        {
+            return length * width;
+        }
+        public double PerimeterOfSquare(double length)
+        {
+            return 4 * length; 
+        }
+        public static void Main()
+        {
+        }
+    }
+}
